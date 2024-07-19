1eb00ba81924fa669a728ef85ee29e3a7611658e
BruzaTom <thomasagomez77@gmail.com>
2024-07-19 03:01:43 -0500

    created findChars

--- main.py
+++ main.py
@@ -2,10 +2,9 @@ def main():
     filePath = "books/frankenstein.txt"
     text = getText(filePath)
     wordCount = wc(text)
-    charCount = cc(text)
+    #charCount = findChars(text) #debug
     print(f"there are {wordCount} words in the frankenstein book")
-    #print(f"there are {wordCount} words in the frankenstein book")
-    print(charCount)
+    #print(charCount) #debug
 
     return
 
@@ -18,26 +17,20 @@ def getText(path):
         file_contents = f.read() #f.read() turns book text into long string
     return file_contents
 
-def cc(textStr):
+def findChars(textStr):
     lowerStr = textStr.lower()
     chars = list(lowerStr)
     charFound = []
     foundChar = None
     for i in range(0, len(chars)): # go though chars
         foundChar = False
-        #charCount = 0
         for j in range(0, len(charFound)):# compare chars to found chars
             if chars[i] == charFound[j]:
                 foundChar = True # if current char already exists aknowlege
-                #charCount += 1
-
         if foundChar == False: #if char was not found 
             charFound.append(chars[i])
-            print(chars[i])
-            #print(charCount)
-
-
-    return len(charFound)
+            #print(chars[i]) #debug
+    return charFound
 
 
 
